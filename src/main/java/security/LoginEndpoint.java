6ddce14f5e91afa2879f2f45a184cecf2d1414ee
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-04-20 13:25:38 +0200

    fixed login

--- src/main/java/security/LoginEndpoint.java
+++ src/main/java/security/LoginEndpoint.java
@@ -54,9 +54,12 @@ public class LoginEndpoint {
             String token = createToken(username, user.getRolesAsStrings());
             JsonObject responseJson = new JsonObject();
             responseJson.addProperty("username", username);
+            List<String> roles = user.getRolesAsStrings();
+            for (int i = 0; i < roles.size(); i++) {
+                String roleName = "role" + i;
+                responseJson.addProperty(roleName, roles.get(i));
+            }
             responseJson.addProperty("token", token);
-            System.out.println("token");
-            System.out.println(token);
             return Response.ok(new Gson().toJson(responseJson)).build();
 
         } catch (JOSEException | AuthenticationException ex) {
