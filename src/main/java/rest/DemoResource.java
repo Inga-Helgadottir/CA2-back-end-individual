36e13f5dc7b3e4cf61f6daa745aa903b9749b314
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-04-21 13:13:12 +0200

    stuff

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -102,7 +102,7 @@ public class DemoResource {
             conn.setRequestProperty("Accept", "application/json;charset=UTF-8");
             if (conn.getResponseCode() != 200) {
                 throw new RuntimeException("Failed : HTTP Error code : "
-                        + conn.getResponseCode() + conn.getResponseMessage());
+                        + conn.getResponseCode());
             }
             InputStreamReader in = new InputStreamReader(conn.getInputStream());
             BufferedReader br = new BufferedReader(in);
